4eacb8001b3edf4f39b08afd21cf7f8a63ecb6ed
chrisxchoi <chris@choi.is>
2022-06-01 10:47:54 -0700

    Complete assignment

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+  // var childNodes = Array.from(document.body.childNodes);
+
+  var findElementByClassName = function(node) {
+
+    if (node.classList.contains(className)) {
+      result.push(node);
+    }
+    // var nodes = node.childNodes;
+    for (var i = 0; i < node.children.length; i++) {
+      findElementByClassName(node.children[i]);
+    }
+  };
+  findElementByClassName(document.body);
+  console.log(result);
+  return result;
 };
+
+  // initialize empty result array
+  // define helper function that takes in node/root
+    // if classList contains className
+      // push node/root into result
+    // else
+      // recursively check next child
+  // call function
+  // return result
